f29afa403ad1bf8e96c7fa2e9cef06c622bb5884
Wellington Junior <wdiasjunior@gmail.com>
2023-08-04 00:34:17 -0300

    figuring out the firmware. the 5 key does not work for some reason

---
+++ firmware/kb.py
@@ -0,0 +1,22 @@
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
+from kmk.scanners import DiodeOrientation
+
+
+class KMKKeyboard(_KMKKeyboard):
+  col_pins = (board.GP10, board.GP11, board.GP12, board.GP13, board.GP14, board.GP15)
+  row_pins = (board.GP21, board.GP20, board.GP19, board.GP18, board.GP17, board.GP16)
+  # keyboard.row_pins = (board.GP28, board.GP27, board.GP26, board.GP25, board.GP24, board.GP23, board.GP22, board.GP21, board.GP20, board.GP19, board.GP18, board.GP17, board.GP16)
+
+  diode_orientation = DiodeOrientation.COLUMNS
+  # diode_orientation = DiodeOrientation.COL2ROW
+
+  coord_mapping = [
+        0,  1,  2,  3,  4,  5,     44, 43, 42, 41, 40, 39, 38, 37, 36, 35,
+        6,  7,  8,  9, 10, 11,     53, 52, 51, 50, 49, 48, 47, 46,     45,
+       12, 13, 14, 15, 16, 17,     62, 61, 60, 59, 58, 57, 56, 55,     54,
+      18, 19, 20, 21, 22,  23,     70, 69, 68, 67, 66, 65, 64,         63,
+     24, 25, 26, 27,  28,  29,     78, 77, 76, 75, 74,   73,   72,     71,
+    30, 31, 32,     33,    34,     86,     85,   84,   83, 82, 81, 80, 79,
+  ]
---
+++ firmware/main.py
@@ -0,0 +1,58 @@
+import board
+
+from kb import KMKKeyboard
+# from kb import data_pin
+from kmk.keys import KC
+from kmk.modules.layers import Layers
+from kmk.extensions.media_keys import MediaKeys
+from kmk.modules.split import Split, SplitSide, SplitType
+# from kmk.hid import HIDModes
+
+keyboard = KMKKeyboard()
+
+layers = Layers()
+
+# TODO Comment one of these on each side
+split_side = SplitSide.LEFT
+# split_side = SplitSide.RIGHT
+
+data_pin = board.GP1 if split_side == SplitSide.LEFT else board.GP0
+data_pin2 = board.GP0 if split_side == SplitSide.LEFT else board.GP1
+
+split = Split(
+  split_side=split_side,
+  split_type=SplitType.UART,
+  data_pin=data_pin,
+  data_pin2=data_pin2,
+  uart_flip=True,
+  # uart_interval=20,
+  split_target_left=True,
+)
+
+keyboard.modules = [layers, split]
+keyboard.extensions.append(MediaKeys())
+
+# figure out ç - RALT + comma? -> maybe setxkbmap works out of the box?
+
+keyboard.keymap = [
+  [
+    KC.ESC,  KC.F1,   KC.F2,   KC.F3,  KC.F4,  KC.F5,       KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, KC.PSCR, KC.PAUS, KC.DEL,\
+    KC.GRV,  KC.N1,   KC.N2,   KC.N3,  KC.N4,  KC.N5,       KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.MINS, KC.EQL, KC.BSPC,          KC.HOME,\
+    KC.TAB,  KC.Q,    KC.W,    KC.E,   KC.R,   KC.T,        KC.Y, KC.U, KC.I, KC.O, KC.P, KC.LBRC, KC.RBRC, KC.BSLS,              KC.END,\
+    KC.CAPS, KC.A,    KC.S,    KC.D,   KC.F,   KC.G,        KC.H, KC.J, KC.K, KC.L, KC.SCLN, KC.QUOT, KC.ENTER,                   KC.PGUP,\
+    KC.LSFT, KC.Z,    KC.X,    KC.C,   KC.V,   KC.B,        KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLSH, KC.RSFT, KC.UP,                KC.PGDOWN,\
+    KC.LCTL, KC.LCMD, KC.LALT,    KC.SPC,    KC.SPC,        KC.SPC, KC.RALT, KC.MO(1), KC.RCTL, KC.LEFT, KC.DOWN, KC.RIGHT,       KC.RCMD,
+  ],
+  [
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.BRID, KC.BRIU,        KC.MPRV, KC.MPLY, KC.MNXT, KC.MUTE, KC.VOLD, KC.VOLU, KC.NO, KC.NO, KC.NO, KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                    KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                    KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                           KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                           KC.NO,\
+    KC.NO,  KC.NO, KC.NO,     KC.NO,    KC.NO,            KC.NO, KC.NO, KC.TRNS, KC.NO, KC.NO, KC.NO, KC.NO,                         KC.NO,
+  ]
+]
+
+if __name__ == "__main__":
+  keyboard.go()
+  # keyboard.go(hid_type=HIDModes.USB)
