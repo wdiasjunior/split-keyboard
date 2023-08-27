10d2e95d74d0eb471d8d1e3b7a234081d35f1b71
Wellington Junior <wdiasjunior@gmail.com>
2023-08-27 13:42:59 -0300

    finished the firmware

--- firmware/main.py
+++ firmware/main.py
@@ -20,7 +20,7 @@ split = Split(
   data_pin2=board.GP1,
   uart_flip=True,
   use_pio=True,
-  # uart_interval=20,
+  # uart_interval=20, # I have no idea what this does. Apparently it's recommended for split boards?
   split_flip=False,
   split_target_left=True,
 )
@@ -30,14 +30,9 @@ keyboard.extensions.append(MediaKeys())
 
 keyboard.debug_enabled = True
 
-# light up led on capslock
-# right super and 3rd space bar
-# tap dance + macros?
-# fn + right super -> mouse jitter?
-
 keyboard.keymap = [
   [
-    KC.ESC,  KC.F1,   KC.F2,   KC.F3,  KC.F4,  KC.F5,       KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, KC.PSCR, KC.PAUS, KC.DEL,\
+    KC.ESC,  KC.F1,   KC.F2,   KC.F3,  KC.F4,  KC.F5,       KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, KC.SLCK, KC.PSCR, KC.DEL,\
     KC.GRV,  KC.N1,   KC.N2,   KC.N3,  KC.N4,  KC.N5,       KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.MINS, KC.EQL, KC.BSPC,          KC.HOME,\
     KC.TAB,  KC.Q,    KC.W,    KC.E,   KC.R,   KC.T,        KC.Y, KC.U, KC.I, KC.O, KC.P, KC.LBRC, KC.RBRC, KC.BSLS,              KC.END,\
     KC.CAPS, KC.A,    KC.S,    KC.D,   KC.F,   KC.G,        KC.H, KC.J, KC.K, KC.L, KC.SCLN, KC.QUOT, KC.ENTER,                   KC.PGUP,\
@@ -45,12 +40,12 @@ keyboard.keymap = [
     KC.LCTL, KC.LCMD, KC.LALT,    KC.SPC,    KC.SPC,        KC.SPC, KC.RALT, KC.MO(1), KC.RCTL, KC.LEFT, KC.DOWN, KC.RIGHT,       KC.RCMD,
   ],
   [
-    KC.NO,  KC.NO, KC.NO, KC.NO, KC.BRID, KC.BRIU,        KC.MPRV, KC.MPLY, KC.MNXT, KC.MUTE, KC.VOLD, KC.VOLU, KC.NO, KC.NO, KC.NO, KC.NO,\
-    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                    KC.NO,\
-    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                    KC.NO,\
-    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                           KC.NO,\
-    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                           KC.NO,\
-    KC.NO,  KC.NO, KC.NO,     KC.NO,    KC.NO,            KC.NO, KC.NO, KC.TRNS, KC.NO, KC.NO, KC.NO, KC.NO,                         KC.NO,
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.BRID, KC.BRIU,        KC.MPRV, KC.MPLY, KC.MNXT, KC.MUTE, KC.VOLD, KC.VOLU, KC.NO, KC.PAUS, KC.NO, KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                      KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                      KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                             KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                             KC.NO,\
+    KC.NO,  KC.NO, KC.NO,     KC.NO,    KC.NO,            KC.NO, KC.NO, KC.TRNS, KC.NO, KC.NO, KC.NO, KC.NO,                           KC.NO,
   ]
 ]
 
