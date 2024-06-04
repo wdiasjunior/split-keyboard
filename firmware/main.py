a3e43876db0f7aa3f5a5bc5c60ff35bd770d71e6
Wellington Junior <wdiasjunior@gmail.com>
2024-06-04 00:53:55 -0300

    updated firmware to add status led for active layer on right side

--- firmware/main.py
+++ firmware/main.py
@@ -22,7 +22,7 @@ split = Split(
   data_pin2=board.GP1,
   uart_flip=True,
   use_pio=True,
-  # uart_interval=20, # I have no idea what this does. Apparently it's recommended for split boards?
+  uart_interval=10,
   split_flip=False,
   split_target_left=True,
 )
@@ -30,7 +30,7 @@ split = Split(
 leds = LED(led_pin=[board.GP25])
 
 class LEDLockStatus(LockStatus):
-  def set_lock_leds(self):
+  def set_lock_led(self):
     if not isRight:
       if self.get_caps_lock():
         leds.set_brightness(50, leds=[0])
@@ -40,9 +40,24 @@ class LEDLockStatus(LockStatus):
   def after_hid_send(self, sandbox):
     super().after_hid_send(sandbox)
     if self.report_updated:
-      self.set_lock_leds()
+      self.set_lock_led()
 
-keyboard.modules = [layers, split]
+LAYER_0 = KC.TO(0)
+LAYER_2 = KC.TO(2)
+
+def set_layer_led_on(key, keyboard, *args):
+  if isRight:
+    leds.set_brightness(50, leds=[0])
+
+def set_layer_led_off(key, keyboard, *args):
+  if isRight:
+    leds.set_brightness(0, leds=[0])
+
+LAYER_2.after_press_handler(set_layer_led_on)
+LAYER_0.after_press_handler(set_layer_led_off)
+
+keyboard.modules.append(layers)
+keyboard.modules.append(split)
 keyboard.extensions.append(MediaKeys())
 keyboard.extensions.append(leds)
 keyboard.extensions.append(LEDLockStatus())
@@ -50,6 +65,7 @@ keyboard.extensions.append(LEDLockStatus())
 keyboard.debug_enabled = True
 
 keyboard.keymap = [
+  # layer 0
   [
     KC.ESC,  KC.F1,   KC.F2,   KC.F3,  KC.F4,  KC.F5,       KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, KC.SLCK, KC.PSCR, KC.DEL,\
     KC.GRV,  KC.N1,   KC.N2,   KC.N3,  KC.N4,  KC.N5,       KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.MINS, KC.EQL, KC.BSPC,          KC.HOME,\
@@ -58,13 +74,23 @@ keyboard.keymap = [
     KC.LSFT, KC.Z,    KC.X,    KC.C,   KC.V,   KC.B,        KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLSH, KC.RSFT, KC.UP,                KC.PGDOWN,\
     KC.LCTL, KC.LCMD, KC.LALT,    KC.SPC,    KC.SPC,        KC.SPC, KC.RALT, KC.MO(1), KC.RCTL, KC.LEFT, KC.DOWN, KC.RIGHT,       KC.RCMD,
   ],
+  # layer 1
   [
-    KC.NO,  KC.NO, KC.NO, KC.NO, KC.BRID, KC.BRIU,        KC.MPRV, KC.MPLY, KC.MNXT, KC.MUTE, KC.VOLD, KC.VOLU, KC.NO, KC.PAUS, KC.NO, KC.NO,\
-    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                      KC.NO,\
-    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                      KC.NO,\
-    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                             KC.NO,\
-    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,          KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                             KC.NO,\
-    KC.NO,  KC.NO, KC.NO,     KC.NO,    KC.NO,            KC.NO, KC.NO, KC.TRNS, KC.NO, KC.NO, KC.NO, KC.NO,                           KC.NO,
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.BRID, KC.BRIU,          KC.MPRV, KC.MPLY, KC.MNXT, KC.MUTE, KC.VOLD, KC.VOLU, LAYER_0, KC.PAUS, KC.NO,  KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,            KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, LAYER_2, KC.NO,                       KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,            KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                         KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,            KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                                KC.NO,\
+    KC.NO,  KC.NO, KC.NO, KC.NO, KC.NO,   KC.NO,            KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,                                KC.NO,\
+    KC.NO,  KC.NO, KC.NO,     KC.NO,    KC.NO,              KC.NO, KC.NO, KC.TRNS, KC.NO, KC.NO, KC.NO, KC.NO,                              KC.NO,
+  ],
+  # layer 2 - same as layer 0, but swaps L-super with L-ctrl
+  [
+    KC.ESC,  KC.F1,   KC.F2,   KC.F3,  KC.F4,  KC.F5,       KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, KC.SLCK, KC.PSCR, KC.DEL,\
+    KC.GRV,  KC.N1,   KC.N2,   KC.N3,  KC.N4,  KC.N5,       KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.MINS, KC.EQL, KC.BSPC,          KC.HOME,\
+    KC.TAB,  KC.Q,    KC.W,    KC.E,   KC.R,   KC.T,        KC.Y, KC.U, KC.I, KC.O, KC.P, KC.LBRC, KC.RBRC, KC.BSLS,              KC.END,\
+    KC.CAPS, KC.A,    KC.S,    KC.D,   KC.F,   KC.G,        KC.H, KC.J, KC.K, KC.L, KC.SCLN, KC.QUOT, KC.ENTER,                   KC.PGUP,\
+    KC.LSFT, KC.Z,    KC.X,    KC.C,   KC.V,   KC.B,        KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLSH, KC.RSFT, KC.UP,                KC.PGDOWN,\
+    KC.LCMD, KC.LCTL, KC.LALT,    KC.SPC,    KC.SPC,        KC.SPC, KC.RALT, KC.MO(1), KC.RCTL, KC.LEFT, KC.DOWN, KC.RIGHT,       KC.RCMD,
   ]
 ]
 
